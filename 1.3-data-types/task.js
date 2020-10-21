4044c0772f8c40469c2ecf9b759b049afe8e3e1b
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2020-10-21 23:36:51 +0300

    fixed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,16 +1,26 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    const parsed = parseInt(percent,contribution,amount);
-    if (isNaN(parsed)) {
-        return "Введите числовое значение!";
+    // const parsed = parseInt(percent,contribution,amount);
+    // if (isNaN(parsed)) {
+    //   return parsed * 100;
+    if (typeof(percent) === 'undefined' && typeof(contribution) === 'undefined' && 
+    typeof(amount) === 'unefined' ){
+      return "Введите числовое значение!";
+    }
+    else if(percent === null && contribution === null && amount === null)
+    {
+      return "Введите числовое значение!";
+    }
+    else if (isNaN(percent) && isNaN(contribution) && isNaN(amount)){
+      return "Введите числовое значение!";
     }
     let S = amount - contribution;
     let n = 0;
     let now = new Date ();
     let nowMonth = now.getMonth();
     let creditMonth = date.getMonth();
-    let nowYear = now.getYear();
-    let creditYear = date.getYear();
+    let nowYear = now.getFullYear();
+    let creditYear = date.getFullYear();
     if (creditYear === 120) {
         n = creditMonth - nowMonth;
     }
@@ -18,21 +28,18 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
         }
     let P = 1/12 * (percent / 100); 
     let paiment = S * (P + P / (Math.pow((1+P),n)- 1));
-    let totalAmount = paiment * n;
-    console.log (totalAmount.toFixed(2));
-    return (totalAmount.toFixed(2));
+    let PretotalAmount = paiment * n;
+    let totalAmount = Number(PretotalAmount.toFixed(2));
+    console.log (totalAmount);
+    return (totalAmount);
   }
 
 function getGreeting(name) {
     let namegreeting = name;
-    if (namegreeting == ""){
+    if (namegreeting === "" || namegreeting === null || typeof(namegreeting) === 'undefined'){
          namegreeting = 'Аноним';
-           console.log(`Привет, меня зовут ${namegreeting}`);
-    } 
-    else { 
-    console.log(`Привет, меня зовут ${namegreeting}`);
-    }
-    let greeting = (`Привет, меня зовут ${namegreeting}`);
-      return greeting;
-    }
+        }
+    let greeting = (`Привет, мир! Меня зовут ${namegreeting}.`);
+    return greeting;
+   }
     
