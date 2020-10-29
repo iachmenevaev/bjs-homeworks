56bda08044b1629785cd2b190353ddea6053a8e5
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2020-10-29 14:33:46 +0300

    2.2object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,9 +1,31 @@
-//String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = 
+ function isPalindrome() {
+   let String = this.toLowerCase();
+    let n = String.length;
+    for (let i = 0; i < n; i++)
+        if (String [i] === String[ n - i - 1]){
+            return true;
+        }
+        else {
+            return false;
+        }
 }
+console.log("А роза упала на лапу Азора")
+//
+//задача 2
+function getAverageMark(marks) {
+    let sum = 0;
+       if (marks.length === 0) {
+       return 0 ;
+       }
+       for (let i = 0; i < marks.length; i++){
+        sum =sum + marks[i] ;
+       }   
+        let aver = sum/marks.length;
+        let averageMark = Math.round(aver);
+        return averageMark;
+    }   
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
